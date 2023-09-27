095eef1f96f6cbbe4e433fbc09362168fec0ae2c
emilywin825 <emilywin825@naver.com>
2023-09-27 23:07:03 +0900

    [init]

---
+++ src/main/java/com/kusithm/meetupd/MeetupDApplication.java
@@ -0,0 +1,13 @@
+package com.kusithm.meetupd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetupDApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetupDApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kusithm/meetupd/MeetupDApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kusithm.meetupd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetupDApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
