0522ca40cab0f302c193e0b129d69c76cad67b29
OJOJIN <ojy101039@gmail.com>
2023-10-18 01:01:41 +0900

    [feat] jwt 토큰 기본 dto 및 provider 설정

---
+++ src/main/java/com/kusithm/meetupd/common/auth/AuthConstants.java
@@ -0,0 +1,6 @@
+package com.kusithm.meetupd.common.auth;
+
+public final class AuthConstants {
+    public static final String AUTH_HEADER = "Authorization";
+    public static final String TOKEN_TYPE = "Bearer ";
+}
