6c391c0adcadc9aa11bf2c708027d204f5157548
OJOJIN <ojy101039@gmail.com>
2023-10-20 04:59:16 +0900

    [fix] 토큰 expire단위 변경

--- src/main/java/com/kusithm/meetupd/common/jwt/TokenProvider.java
+++ src/main/java/com/kusithm/meetupd/common/jwt/TokenProvider.java
@@ -44,7 +44,7 @@ public class TokenProvider {
                 .signWith(new SecretKeySpec(secretKey.getBytes(), SignatureAlgorithm.HS512.getJcaName()))   // HS512 알고리즘을 사용하여 secretKey를 이용해 서명
                 .setSubject(String.valueOf(userId))  // JWT 토큰 제목
                 .setIssuedAt(Timestamp.valueOf(LocalDateTime.now()))    // JWT 토큰 발급 시간
-                .setExpiration(Date.from(Instant.now().plus(accessExpirationHours, ChronoUnit.SECONDS)))    // JWT 토큰 만료 시간
+                .setExpiration(Date.from(Instant.now().plus(accessExpirationHours, ChronoUnit.HOURS)))    // JWT 토큰 만료 시간
                 .compact(); // JWT 토큰 생성
     }
 
@@ -52,7 +52,7 @@ public class TokenProvider {
     public String createRefreshToken() {
         return Jwts.builder()
                 .signWith(new SecretKeySpec(secretKey.getBytes(), SignatureAlgorithm.HS512.getJcaName()))   // HS512 알고리즘을 사용하여 secretKey를 이용해 서명
-                .setExpiration(Date.from(Instant.now().plus(refreshExpirationHours, ChronoUnit.SECONDS)))    // JWT 토큰 만료 시간
+                .setExpiration(Date.from(Instant.now().plus(refreshExpirationHours, ChronoUnit.HOURS)))    // JWT 토큰 만료 시간
                 .compact(); // JWT 토큰 생성
     }
 
