0580c397e0b5aeae4474657ba887336c91585a10
OJOJIN <ojy101039@gmail.com>
2023-10-28 01:27:41 +0900

    [feat] 공모전 type enum 생성

---
+++ src/main/java/com/kusithm/meetupd/domain/contest/entity/ContestType.java
@@ -0,0 +1,24 @@
+package com.kusithm.meetupd.domain.contest.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum ContestType {
+
+    PLAN_IDEA(1, "기획/아이디어"),
+    BRAND_NAMING(2, "브랜드/네이밍"),
+    AD_MARKETING(3, "광고/마케팅"),
+    IMAGE_VIDEO_UCC(4, "사진/영상/UCC"),
+    DESIGN(5,"디자인"),
+    ART_PHYSICAL_EDU(6, "예체능"),
+    POEM(7, "문학/수기/시나리오"),
+    IT(8, "IT/소프트웨어/게임"),
+    STARTUP(9, "창업/스타트업"),
+    ETC(10, "기타");
+
+    private final Integer idx;
+
+    private final String desc;
+}
