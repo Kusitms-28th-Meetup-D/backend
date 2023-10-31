93b156c8a52de8c1250c4cd974fc52f81121b7c6
OJOJIN <ojy101039@gmail.com>
2023-11-01 02:01:34 +0900

    [feat] Contest entity 내에 서비스에 필요한 정보들 추가

--- src/main/java/com/kusithm/meetupd/domain/contest/entity/Contest.java
+++ src/main/java/com/kusithm/meetupd/domain/contest/entity/Contest.java
@@ -1,29 +1,37 @@
 package com.kusithm.meetupd.domain.contest.entity;
 
 import jakarta.persistence.Id;
+import lombok.Getter;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-@Document(collation = "contest")
+@Getter
+@Document("contest")
 public class Contest {
 
     @Id
+    @Field(name="_id")
     private String id;
 
     @Field(name = "title")
     private String title;   // 공모전 제목
 
-    @Field(name = "type")
-    private Integer type;   // 공모전 분야
+    @Field(name = "company")
+    private String company;   // 공모전 주최사
+
+    @Field(name = "types")
+    private List<Integer> types = new ArrayList<>();   // 공모전 분야
 
     @Field(name = "recruit_start")
-    private Date recruitmentStartDate;  // 지원 시작 일자
+    private LocalDate recruitmentStartDate;  // 지원 시작 일자
 
     @Field(name = "recruit_end")
-    private Date recruitmentEndDate;    // 지원 마감 일자
+    private LocalDate recruitmentEndDate;    // 지원 마감 일자
 
     @Field(name = "qualification")
     private String qualification;   // 지원 자격
@@ -40,6 +48,9 @@ public class Contest {
     @Field(name = "description")
     private String desc;    // 상세 설명
 
+    @Field(name = "team_num")
+    private Integer teamNum;    // 공모전 내 팀 수
+
     @Field(name = "contest_images")
-    private List<ContestImage> contestImages;   // 공모전 사진
+    private List<String> contestImages = new ArrayList<>();   // 공모전 사진
 }
