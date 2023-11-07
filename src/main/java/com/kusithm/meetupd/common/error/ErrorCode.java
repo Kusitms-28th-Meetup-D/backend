5955c00335f4407c2e3dfa705644c151652b4f23
OJOJIN <ojy101039@gmail.com>
2023-11-08 01:44:48 +0900

    [feat] 유저 리뷰 조회 및 등록 API 작성

--- src/main/java/com/kusithm/meetupd/common/error/ErrorCode.java
+++ src/main/java/com/kusithm/meetupd/common/error/ErrorCode.java
@@ -37,6 +37,7 @@ public enum ErrorCode {
     ENUM_NOT_FOUND(HttpStatus.NOT_FOUND, "요청하신 ENUM TYPE을 찾을 수 없습니다."),
 
     CONTEST_NOT_FOUND(HttpStatus.NOT_FOUND, "요청하신 공모전을 찾을 수 없습니다."),
+    USER_RECOMMENDATION_NOT_FOUND(HttpStatus.NOT_FOUND, "요청하신 유저의 추천사를 찾을 수 없습니다."),
 
 
 
