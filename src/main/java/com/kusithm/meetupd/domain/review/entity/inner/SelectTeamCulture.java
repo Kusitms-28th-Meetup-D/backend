549c5c5919f2df72c6f0e9475ae99fce84255d3b
OJOJIN <ojy101039@gmail.com>
2023-11-08 01:42:52 +0900

    [feat] WaitReview 및 내부 entity 생성

---
+++ src/main/java/com/kusithm/meetupd/domain/review/entity/inner/SelectTeamCulture.java
@@ -0,0 +1,29 @@
+package com.kusithm.meetupd.domain.review.entity.inner;
+
+import com.kusithm.meetupd.domain.review.dto.request.UploadReviewRequestDto;
+import com.kusithm.meetupd.domain.review.dto.request.UploadReviewRequestDto.SelectTeamCultureDto;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+@Builder
+@Getter
+public class SelectTeamCulture {
+
+    @Field(name = "feedback_style")
+    private Integer feedbackStyle;
+
+    @Field(name = "team_style")
+    private Integer teamStyle;
+
+    @Field(name = "personality_style")
+    private Integer personalityStyle;
+
+    public static SelectTeamCulture of(SelectTeamCultureDto selectTeamCultureDto) {
+        return SelectTeamCulture.builder()
+                .feedbackStyle(selectTeamCultureDto.getFeedbackStyle())
+                .teamStyle(selectTeamCultureDto.getTeamStyle())
+                .personalityStyle(selectTeamCultureDto.getPersonalityStyle())
+                .build();
+    }
+}
