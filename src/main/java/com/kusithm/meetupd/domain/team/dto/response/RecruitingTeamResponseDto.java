f1d13728c19ab088d9bfe42f4341eb4e642818d5
emilywin825 <emilywin825@naver.com>
2023-11-10 04:41:27 +0900

    [feat] 모집중인 팀 responseDto 생성

---
+++ src/main/java/com/kusithm/meetupd/domain/team/dto/response/RecruitingTeamResponseDto.java
@@ -0,0 +1,46 @@
+package com.kusithm.meetupd.domain.team.dto.response;
+
+import com.kusithm.meetupd.common.error.EnumNotFoundException;
+import com.kusithm.meetupd.domain.team.entity.Team;
+import com.kusithm.meetupd.domain.team.entity.TeamUser;
+import com.kusithm.meetupd.domain.user.entity.User;
+import lombok.*;
+
+import java.util.List;
+
+import static com.kusithm.meetupd.common.error.ErrorCode.ENUM_NOT_FOUND;
+import static com.kusithm.meetupd.domain.team.entity.TeamUserRoleType.TEAM_LEADER;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class RecruitingTeamResponseDto {
+
+    //공모전Id,공모전이름,팀장Id,팀장이름,팀장프로필사진,팀장의 한마디
+    private String contestId;
+
+    private String contesttitle; //공모전 이름
+
+    private Long teamLeaderId;
+
+    private String teamLeaderName;
+
+    private String teamLeaderImage;
+
+    private String teamLeaderMessage;
+
+    public RecruitingTeamResponseDto(Team team, User user) {
+
+        this.teamLeaderId = user.getId();
+
+        this.teamLeaderName = user.getUsername();
+
+        this.teamLeaderImage = user.getProfileImage();
+
+        this.teamLeaderMessage = team.getLeaderMessage();
+
+    }
+}
