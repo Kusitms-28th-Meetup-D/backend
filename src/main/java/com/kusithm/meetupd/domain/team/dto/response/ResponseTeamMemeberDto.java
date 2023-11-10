ca5bb43f476809bdf22af6d694efa506988d45a5
emilywin825 <emilywin825@naver.com>
2023-11-11 03:36:51 +0900

    [feat] 팀관련 dto 생성

---
+++ src/main/java/com/kusithm/meetupd/domain/team/dto/response/ResponseTeamMemeberDto.java
@@ -0,0 +1,9 @@
+package com.kusithm.meetupd.domain.team.dto.response;
+
+public class ResponseTeamMemeberDto {
+    private Long teamMemberId;
+    private String teamMemberImage;
+    private String teamMemberName;
+    private String teamMemberTask;
+    private String teamMemberMajor;
+}
