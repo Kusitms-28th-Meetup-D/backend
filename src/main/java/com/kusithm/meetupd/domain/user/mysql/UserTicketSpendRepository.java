cf4e7e6fd373322c2a82a0ed25b4878f72997890
OJOJIN <ojy101039@gmail.com>
2023-11-12 03:47:23 +0900

    [feat] UserTicketSpend 레포 생성

---
+++ src/main/java/com/kusithm/meetupd/domain/user/mysql/UserTicketSpendRepository.java
@@ -0,0 +1,12 @@
+package com.kusithm.meetupd.domain.user.mysql;
+
+import com.kusithm.meetupd.domain.user.entity.UserTicketSpend;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserTicketSpendRepository extends JpaRepository<UserTicketSpend, Long> {
+
+
+    Boolean existsByUserIdAndPurchaseUserId(Long userId, Long purchaseUserId);
+
+//    Boolean existsByBuyUserIdAndSoldUserId(Long buyUserId, Long soldUserId);
+}
