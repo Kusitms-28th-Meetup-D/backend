53ab85435ae3a53b4ed415d7dfd90c8626ff27ea
emilywin825 <emilywin825@naver.com>
2023-11-12 06:44:09 +0900

    [feat] 공모전내에서 모집중인 팀 white-list 추가

--- src/main/java/com/kusithm/meetupd/common/auth/JwtAuthenticationFilter.java
+++ src/main/java/com/kusithm/meetupd/common/auth/JwtAuthenticationFilter.java
@@ -45,7 +45,8 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             new AntPathRequestMatcher("/api/reviews"),
             new AntPathRequestMatcher("/api/reviews/check-reviewed"),
             new AntPathRequestMatcher(("/api/reviews/info/*")),
-            new AntPathRequestMatcher(("/api/teams/recruiting"))
+            new AntPathRequestMatcher(("/api/teams/recruiting")),
+            new AntPathRequestMatcher(("/api/teams/contest/*"))
     );
 
     @Override
