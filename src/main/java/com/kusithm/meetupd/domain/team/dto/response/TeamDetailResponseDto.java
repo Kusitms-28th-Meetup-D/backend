7f714b5cb1903b6794b5e69ce09ce0331070f60e
emilywin825 <emilywin825@naver.com>
2023-11-12 15:47:53 +0900

    [refactor] 팀 상세조회 dto 수정 - 기획팀 요청으로 개발 중단

--- src/main/java/com/kusithm/meetupd/domain/team/dto/response/TeamDetailResponseDto.java
+++ src/main/java/com/kusithm/meetupd/domain/team/dto/response/TeamDetailResponseDto.java
@@ -5,15 +5,13 @@ import java.util.List;
 
 public class TeamDetailResponseDto {
 
-    public String contestId; //공모전 Id
-
-    public ResponseTeamLeaderDto leaderDto;
+    public ResponseTeamLeaderDto leaderInfo;
 
     public int expectedMember;//모집할 인원수
     public int joinMember;//합류한 인원수
     public String location;//활동지역
     public String endDate;//활동 종료 예정일
-    //모집공고
+    public String notice;//모집공고
     public int leftMember;//남은 자리수 : 모집할 인원수 - 모집된 인원수
-    public List<ResponseTeamMemeberDto> teamMemeberDtos;
+    public List<ResponseTeamMemeberDto> teamMemeberInfos;
 }
