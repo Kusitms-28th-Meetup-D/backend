194880e9f9d3a79c278cf7ca7d3e6592b16f53be
emilywin825 <emilywin825@naver.com>
2023-11-14 17:35:08 +0900

    [feat] TeamUser 엔터티 생성 함수 추가

--- src/main/java/com/kusithm/meetupd/domain/team/entity/TeamUser.java
+++ src/main/java/com/kusithm/meetupd/domain/team/entity/TeamUser.java
@@ -1,12 +1,21 @@
 package com.kusithm.meetupd.domain.team.entity;
 
 import com.kusithm.meetupd.common.entity.BaseEntity;
+import com.kusithm.meetupd.domain.user.entity.Location;
 import com.kusithm.meetupd.domain.user.entity.User;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static com.kusithm.meetupd.domain.team.entity.TeamProgressType.RECRUITING;
 
 @Entity
 @Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class TeamUser extends BaseEntity {
 
     @Id
@@ -24,4 +33,13 @@ public class TeamUser extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
+
+    public static TeamUser toEntity(Integer role, Team team, User user) {
+        return TeamUser.builder()
+                .role(role)
+                .team(team)
+                .user(user)
+                .build();
+    }
 }
+
