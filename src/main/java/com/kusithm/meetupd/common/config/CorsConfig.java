cc1b473e1ce14ab80a24d7d682e49f98c9dbf766
OJOJIN <ojy101039@gmail.com>
2023-11-17 03:52:23 +0900

    [feat] corsConfig 추가

---
+++ src/main/java/com/kusithm/meetupd/common/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.kusithm.meetupd.common.config;
+
+import org.apache.catalina.filters.CorsFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:5173");
+        config.addAllowedOrigin("https://meetup-d.vercel.app");
+        config.addAllowedHeader("*");
+        config.setAllowedMethods(Arrays.asList("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+        source.registerCorsConfiguration("/**",config);
+        return new CorsFilter();
+    }
+}
