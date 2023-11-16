ea67d737be892384ee3bb0947438f6e7dc6d4176
OJOJIN <ojy101039@gmail.com>
2023-11-17 04:23:22 +0900

    [fix] corsFilter 포트 변경

--- src/main/java/com/kusithm/meetupd/common/config/CorsConfig.java
+++ src/main/java/com/kusithm/meetupd/common/config/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("http://localhost:3000");
+        config.addAllowedOrigin("http://localhost:5173");
         config.addAllowedOrigin("https://meetup-d.vercel.app");
         config.addAllowedHeader("*");
         config.setAllowedMethods(Arrays.asList("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
