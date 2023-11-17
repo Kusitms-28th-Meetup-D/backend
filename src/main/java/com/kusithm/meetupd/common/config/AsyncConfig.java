12db68d0ee2174da0a8cf83d7b7327a2bd675d17
OJOJIN <ojy101039@gmail.com>
2023-11-17 22:55:32 +0900

    [feat] AsyncConfig 추가

---
+++ src/main/java/com/kusithm/meetupd/common/config/AsyncConfig.java
@@ -0,0 +1,25 @@
+package com.kusithm.meetupd.common.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurerSupport;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig extends AsyncConfigurerSupport {
+
+    @Override
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(20);
+        executor.setQueueCapacity(30);
+        executor.setThreadNamePrefix("WANTEAM-ASYNC-");
+        executor.initialize();
+        return executor;
+    }
+}
